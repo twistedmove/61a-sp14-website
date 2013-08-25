43050ea010f1c880f9f243dd209cf3d62efd7449
Mark Miyashita <negativetwelve@gmail.com>
2013-08-25 12:07:43 -0700

    Adding lab 0 and most of lab 1

---
+++ lab/lab01/hw0.py
@@ -0,0 +1,41 @@
+# Name:
+# Login:
+# TA:
+# Section:
+
+
+def my_name():
+    """Returns a string that contains your first and last name.
+    >>> my_name() != 'PUT YOUR FULL NAME HERE'
+    True
+    >>> type(my_name()) == str
+    True
+    """
+    return 'PUT YOUR FULL NAME HERE'
+
+def my_sid():
+    """Returns a number that is your student ID.
+    >>> my_sid() != 99999999
+    True
+    >>> type(my_sid()) == int
+    True
+    """
+    return 99999999
+
+def my_email():
+    """Returns a string that contains your email address.
+    >>> my_email() != 'put_your_email_here@berkeley.edu'
+    True
+    >>> type(my_email()) == str
+    True
+    """
+    return 'put_your_email_here@berkeley.edu'
+
+def my_login():
+    """Returns a string that represents your login. Your login should start with cs61a-
+    >>> my_login() != 'cs61a-??'
+    True
+    >>> type(my_login()) == str
+    True
+    """
+    return 'cs61a-??'
