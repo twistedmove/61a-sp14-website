64de3c0b7af67a8311b6c44e9f3140d073e8f5ac
John DeNero <papajohn@gmail.com>
2013-10-03 12:56:41 -0700

    hw 3 solutions

---
+++ hw/solutions/hw3_solutions.py
@@ -0,0 +1,189 @@
+# Name:
+# Email:
+
+# Q1.
+
+def g(n):
+    """Return the value of G(n), computed recursively.
+
+    >>> g(1)
+    1
+    >>> g(2)
+    2
+    >>> g(3)
+    3
+    >>> g(4)
+    10
+    >>> g(5)
+    22
+    """
+    if n in (1, 2, 3):
+        return n
+    return g(n-1) + 2*g(n-2) + 3*g(n-3)
+
+def g_iter(n):
+    """Return the value of G(n), computed iteratively.
+
+    >>> g_iter(1)
+    1
+    >>> g_iter(2)
+    2
+    >>> g_iter(3)
+    3
+    >>> g_iter(4)
+    10
+    >>> g_iter(5)
+    22
+    """
+    if n == 1 or n == 2 or n == 3:
+        return n
+    a, b, c = 1, 2, 3
+    while n > 3:
+        a, b, c = b, c, c + 2*b + 3*a
+        n = n - 1
+    return c
+
+# Q2.
+
+def has_seven(k):
+    """Has a has_seven
+    >>> has_seven(3)
+    False
+    >>> has_seven(7)
+    True
+    >>> has_seven(2734)
+    True
+    >>> has_seven(2634)
+    False
+    >>> has_seven(734)
+    True
+    >>> has_seven(7777)
+    True
+    """
+    if k % 10 == 7:
+        return True
+    elif k < 10:
+        return False
+    else:
+        return has_seven(k // 10)
+
+# Q3.
+
+"1 2 3 4 5 6 [7] 6 5 4 3 2 1 [0] 1 2 [3] 2 1 0 [-1] 0 1 2 3 4 [5] [4] 5 6"
+
+
+def pingpong(n):
+    """Return the nth element of the ping-pong sequence.
+
+    >>> pingpong(7)
+    7
+    >>> pingpong(8)
+    6
+    >>> pingpong(15)
+    1
+    >>> pingpong(21)
+    -1
+    >>> pingpong(22)
+    0
+    >>> pingpong(30)
+    6
+    >>> pingpong(68)
+    2
+    >>> pingpong(69)
+    1
+    >>> pingpong(70)
+    0
+    >>> pingpong(71)
+    1
+    >>> pingpong(72)
+    0
+    >>> pingpong(100)
+    2
+    """
+    def pingpong_next(k, p, up):
+        if k == n:
+            return p
+        if up:
+            return pingpong_switch(k+1, p+1, up)
+        else:
+            return pingpong_switch(k+1, p-1, up)
+
+    def pingpong_switch(k, p, up):
+        if k % 7 == 0 or has_seven(k):
+            return pingpong_next(k, p, not up)
+        else:
+            return pingpong_next(k, p, up)
+
+    return pingpong_next(1, 1, True)
+
+# Q4.
+
+def ten_pairs(n):
+    """Return the number of ten-pairs within positive integer n.
+
+    >>> ten_pairs(7823952)
+    3
+    >>> ten_pairs(55055)
+    6
+    >>> ten_pairs(9641469)
+    6
+    """
+    if n < 10:
+        return 0
+    else:
+        return ten_pairs(n//10) + count_digit(n//10, 10 - n%10)
+
+def count_digit(n, digit):
+    """Return how many times digit appears in n.
+
+    >>> count_digit(55055, 5)
+    4
+    """
+    if n == 0:
+        return 0
+    else:
+        if n%10 == digit:
+            return count_digit(n//10, digit) + 1
+        else:
+            return count_digit(n//10, digit)
+
+# Q5.
+
+def count_change(amount):
+    """Return the number of ways to make change for amount.
+
+    >>> count_change(7)
+    6
+    >>> count_change(10)
+    14
+    >>> count_change(20)
+    60
+    >>> count_change(100)
+    9828
+    """
+    return count_using(1, amount)
+
+def count_using(min_coin, amount):
+    if amount < 0:
+        return 0
+    elif amount == 0:
+        return 1
+    elif min_coin > amount:
+        return 0
+    else:
+        with_min = count_using(min_coin, amount - min_coin)
+        without_min = count_using(2*min_coin, amount)
+        return with_min + without_min
+
+# Q6.
+
+from operator import sub, mul
+
+def make_anonymous_factorial():
+    """Return the value of an expression that computes factorial.
+
+    >>> make_anonymous_factorial()(5)
+    120
+    """
+    return (lambda f: lambda k: f(f, k))(lambda f, k: k if k == 1 else mul(k, f(f, sub(k, 1))))
+
