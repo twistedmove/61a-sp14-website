9d037b7cf6da021a7f5ac57b76f6b4efb91df358
Brian Hou <brian.hou@live.com>
2013-10-09 09:52:27 -0700

    Add hw4 solutions

---
+++ hw/solutions/hw4_solutions.py
@@ -0,0 +1,332 @@
+# Name:
+# Email:
+
+
+def str_interval(x):
+    """Return a string representation of interval x.
+
+    >>> str_interval(interval(-1, 2))
+    '-1 to 2'
+    """
+    return '{0} to {1}'.format(lower_bound(x), upper_bound(x))
+
+def add_interval(x, y):
+    """Return an interval that contains the sum of any value in interval x and
+    any value in interval y.
+
+    >>> str_interval(add_interval(interval(-1, 2), interval(4, 8)))
+    '3 to 10'
+    """
+    lower = lower_bound(x) + lower_bound(y)
+    upper = upper_bound(x) + upper_bound(y)
+    return interval(lower, upper)
+
+def mul_interval(x, y):
+    """Return the interval that contains the product of any value in x and any
+    value in y.
+
+    >>> str_interval(mul_interval(interval(-1, 2), interval(4, 8)))
+    '-8 to 16'
+    """
+    p1 = lower_bound(x) * lower_bound(y)
+    p2 = lower_bound(x) * upper_bound(y)
+    p3 = upper_bound(x) * lower_bound(y)
+    p4 = upper_bound(x) * upper_bound(y)
+    return interval(min(p1, p2, p3, p4), max(p1, p2, p3, p4))
+
+
+# Q1.
+
+def interval(a, b):
+    """Construct an interval from a to b."""
+    return (a, b)
+
+def lower_bound(x):
+    """Return the lower bound of interval x."""
+    return x[0]
+
+def upper_bound(x):
+    """Return the upper bound of interval x."""
+    return x[1]
+
+# Q2.
+
+def div_interval(x, y):
+    """Return the interval that contains the quotient of any value in x divided
+    by any value in y.
+
+    Division is implemented as the multiplication of x by the reciprocal of y.
+
+    >>> str_interval(div_interval(interval(-1, 2), interval(4, 8)))
+    '-0.25 to 0.5'
+    """
+    assert lower_bound(y) > 0 or upper_bound(y) < 0, 'Divide by zero'
+    reciprocal_y = interval(1/upper_bound(y), 1/lower_bound(y))
+    return mul_interval(x, reciprocal_y)
+
+# Q3.
+
+def sub_interval(x, y):
+    """Return the interval that contains the difference between any value in x
+    and any value in y.
+
+    >>> str_interval(sub_interval(interval(-1, 2), interval(4, 8)))
+    '-9 to -2'
+    """
+    negative_y = interval(-upper_bound(y), -lower_bound(y))
+    return add_interval(x, negative_y)
+
+
+# Q4.
+
+def mul_interval_fast(x, y):
+    """Return the interval that contains the product of any value in x and any
+    value in y, using as few multiplications as possible.
+
+    >>> str_interval(mul_interval_fast(interval(-1, 2), interval(4, 8)))
+    '-8 to 16'
+    >>> str_interval(mul_interval_fast(interval(-2, -1), interval(4, 8)))
+    '-16 to -4'
+    >>> str_interval(mul_interval_fast(interval(-1, 3), interval(-4, 8)))
+    '-12 to 24'
+    >>> str_interval(mul_interval_fast(interval(-1, 2), interval(-8, 4)))
+    '-16 to 8'
+    """
+    lx, ux = lower_bound(x), upper_bound(x)
+    ly, uy = lower_bound(y), upper_bound(y)
+    if lx >= 0 and ly >= 0: # ux and uy must also be positive
+        return interval(lx * ly, ux * uy)
+    elif lx < 0 and ly >= 0: # uy must also be positive
+        if ux >= 0:
+            return interval(lx * uy, ux * uy)
+        else:
+            return interval(lx * uy, ux * ly)
+    elif lx >= 0 and ly < 0: # ux must also be positive
+        if uy >= 0:
+            return interval(ux * ly, ux * uy)
+        else:
+            return interval(ux * ly, lx * uy)
+    else:
+        if ux >= 0 and uy >= 0:
+            return interval(min(lx*uy, ux*ly), max(lx*ly, ux*uy))
+        elif ux < 0 and uy >= 0:
+            return interval(lx * uy, lx * ly)
+        elif ux >= 0 and uy < 0:
+            return interval(ux * ly, lx * ly)
+        else:
+            return interval(ux * uy, lx * ly)
+
+
+# Q5.
+
+def make_center_width(c, w):
+    """Construct an interval from center and width."""
+    return interval(c - w, c + w)
+
+def center(x):
+    """Return the center of interval x."""
+    return (upper_bound(x) + lower_bound(x)) / 2
+
+def width(x):
+    """Return the width of interval x."""
+    return (upper_bound(x) - lower_bound(x)) / 2
+
+
+def make_center_percent(c, p):
+    """Construct an interval from center and percentage tolerance.
+
+    >>> str_interval(make_center_percent(2, 50))
+    '1.0 to 3.0'
+    """
+    return make_center_width(c, c*p/100)
+
+def percent(x):
+    """Return the percentage tolerance of interval x.
+
+    >>> percent(interval(1, 3))
+    50.0
+    """
+    return 100 * width(x) / center(x)
+
+# Q6.
+
+def par1(r1, r2):
+    return div_interval(mul_interval(r1, r2), add_interval(r1, r2))
+
+def par2(r1, r2):
+    one = interval(1, 1)
+    rep_r1 = div_interval(one, r1)
+    rep_r2 = div_interval(one, r2)
+    return div_interval(one, add_interval(rep_r1, rep_r2))
+
+
+# These two intervals give different results for parallel resistors:
+a = make_center_percent(1, 1)
+b = make_center_percent(2, 1)
+print(str_interval(par1(a, b)), '!=', str_interval(par2(a, b)))
+
+# Q7.
+
+def multiple_references_explanation():
+  return """The multiple reference problem exists.  The true value
+  within a particular interval is fixed (though unknown).  Nested
+  combinations that refer to the same interval twice may assume two different
+  true values for the same interval, which is an error that results in
+  intervals that are larger than they should be.
+
+  Consider the case of i * i, where i is an interval from -1 to 1.  No value
+  within this interval, when squared, will give a negative result.  However,
+  our mul_interval function will allow us to choose 1 from the first
+  reference to i and -1 from the second, giving an erroneous lower bound of
+  -1.
+
+  Hence, a program like par2 is better than par1 because it never combines
+  the same interval more than once.
+  """
+
+# Q8.
+
+
+def quadratic(x, a, b, c):
+    """Return the interval that is the range of the quadratic defined by
+    coefficients a, b, and c, for domain interval x.
+
+    >>> str_interval(quadratic(interval(0, 2), -2, 3, -1))
+    '-3 to 0.125'
+    >>> str_interval(quadratic(interval(1, 3), 2, -3, 1))
+    '0 to 10'
+    """
+    extremum = -b / (2*a)
+    f = lambda x: a * x * x + b * x + c
+    l, u, e = map(f, (lower_bound(x), upper_bound(x), extremum))
+    if extremum >= lower_bound(x) and extremum <= upper_bound(x):
+        return interval(min(l, u, e), max(l, u, e))
+    else:
+        return interval(min(l, u), max(l, u))
+
+# Q9.
+
+def non_zero(x):
+    """Return whether x contains 0."""
+    return lower_bound(x) > 0 or upper_bound(x) < 0
+
+def square_interval(x):
+    """Return the interval that contains all squares of values in x, where x
+    does not contain 0.
+    """
+    assert non_zero(x), 'square_interval is incorrect for x containing 0'
+    return mul_interval(x, x)
+
+# The first two of these intervals contain 0, but the third does not.
+seq = (interval(-1, 2), make_center_width(-1, 2), make_center_percent(-1, 50))
+
+zero = interval(0, 0)
+
+def sum_nonzero_with_for(seq):
+    """Returns an interval that is the sum of the squares of the non-zero
+    intervals in seq, using a for statement.
+
+    >>> str_interval(sum_nonzero_with_for(seq))
+    '0.25 to 2.25'
+    """
+    total = zero
+    for x in seq:
+        if non_zero(x):
+            total = add_interval(total, square_interval(x))
+    return total
+
+from functools import reduce
+def sum_nonzero_with_map_filter_reduce(seq):
+    """Returns an interval that is the sum of the squares of the non-zero
+    intervals in seq, using using map, filter, and reduce.
+
+    >>> str_interval(sum_nonzero_with_map_filter_reduce(seq))
+    '0.25 to 2.25'
+    """
+    elements = map(square_interval, filter(non_zero, seq))
+    return reduce(add_interval, elements, zero)
+
+def sum_nonzero_with_generator_reduce(seq):
+    """Returns an interval that is the sum of the squares of the non-zero
+    intervals in seq, using using reduce and a generator expression.
+
+    >>> str_interval(sum_nonzero_with_generator_reduce(seq))
+    '0.25 to 2.25'
+    """
+    elements = (square_interval(x) for x in seq if non_zero(x))
+    return reduce(add_interval, elements, zero)
+
+
+# Q10.
+
+def polynomial(x, c):
+    """Return the interval that is the range of the polynomial defined by
+    coefficients c, for domain interval x.
+
+    >>> str_interval(polynomial(interval(0, 2), (-1, 3, -2)))
+    '-3 to 0.125'
+    >>> str_interval(polynomial(interval(1, 3), (1, -3, 2)))
+    '0 to 10'
+    >>> str_interval(polynomial(interval(0.5, 2.25), (10, 24, -6, -8, 3)))
+    '18.0 to 23.0'
+    """
+    def add_fn(coeff, k, f):
+        return lambda x: coeff * pow(x, k) + f(x)
+
+    def add_dfn(coeff, k, df):
+        return lambda x: k * coeff * pow(x, k-1) + df(x)
+
+    def add_ddfn(coeff, k, ddf):
+        return lambda x: k * (k-1) * coeff * pow(x, k-2) + ddf(x)
+
+    # Define the polynomial and its first and second derivatives.
+    f = lambda x: 0
+    df = lambda x: 0
+    ddf = lambda x: 0
+    for k, coeff in enumerate(c):
+        f = add_fn(coeff, k, f)
+        if k > 0:
+            df = add_dfn(coeff, k, df)
+        if k > 1:
+            ddf = add_ddfn(coeff, k, ddf)
+
+    # Find as many extreme points as we can using Newton's method
+    lower, upper = lower_bound(x), upper_bound(x)
+    num_steps = 20
+    step = (upper - lower) / num_steps
+    starts = tuple(lower + k * step for k in range(num_steps))
+    extremums = tuple(find_zero(df, ddf, n) for n in starts)
+
+    # Filter for the interval x and return
+    ns = tuple(n for n in extremums if n > lower and n < upper) + (lower, upper)
+    values = [f(n) for n in ns]
+    return interval(min(values), max(values))
+
+# Newton's method from lecture
+
+def improve(update, close, guess=1, max_updates=100):
+    """Iteratively improve guess with update until close(guess) is true or
+    max_updates have been applied."""
+    k = 0
+    while not close(guess) and k < max_updates:
+        guess = update(guess)
+        k = k + 1
+    return guess
+
+def approx_eq(x, y, tolerance=1e-15):
+    return abs(x - y) < tolerance
+
+def find_zero(f, df, guess=1):
+    """Return a zero of the function f with derivative df."""
+    def near_zero(x):
+        return approx_eq(f(x), 0)
+    return improve(newton_update(f, df), near_zero, guess)
+
+def newton_update(f, df):
+    """Return an update function for f with derivative df,
+    using Newton's method."""
+    def update(x):
+        return x - f(x) / df(x)
+    return update
+
+
