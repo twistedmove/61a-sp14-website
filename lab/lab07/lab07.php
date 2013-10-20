3b74618fb67f347e43ea4b6fb203bdbaf9a26868
Brian Hou <brian.hou@live.com>
2013-10-20 02:56:55 -0700

    Update lab7 + add starter file

--- lab/lab07/lab07.php
+++ lab/lab07/lab07.php
@@ -48,356 +48,343 @@
   <body style="font-family: Georgia,serif;">
     <h1>CS 61A Lab 7</h1>
 <h2>Sets & Orders of Growth</h2>
-<h2>Sets</h2>
+<p>We've provided a starter file with skeleton code for the exercises in the
+lab. You can get it at the following link:</p>
 
-<p>A set is an unordered collection of distinct objects that supports membership testing, union, intersection, and adjunction.</p>
+<ul>
+<li><a href="./lab7.py">lab7.py</a></li>
+</ul>
+
+<h3>Sets</h3>
+
+<p>A set is an unordered collection of distinct objects that supports membership
+testing, union, intersection, and adjunction.</p>
 
 <p>Construction</p>
 
-<pre>
->>> s = {3, 2, 1, 4, 4}
->>> s
-{1, 2, 3, 4}</pre>
+<pre><code>&gt;&gt;&gt; s = {3, 2, 1, 4, 4}
+&gt;&gt;&gt; s
+{1, 2, 3, 4}
+</code></pre>
 
-Adjunction
+<p>Adjunction</p>
 
-<pre>>>> s.add(5)
->>> s
+<pre><code>&gt;&gt;&gt; s.add(5)
+&gt;&gt;&gt; s
 {1, 2, 3, 4, 5}
-</pre>
+</code></pre>
 
 <p>Operations</p>
 
-<pre>
->>> 3 in s
+<pre><code>&gt;&gt;&gt; 3 in s
 True
->>> 7 not in s
+&gt;&gt;&gt; 7 not in s
 True
->>> len(s)
-4
->>> s.union({1, 5})
+&gt;&gt;&gt; len(s)
+5
+&gt;&gt;&gt; s.union({1, 5})
 {1, 2, 3, 4, 5}
->>> s.intersection({6, 5, 4, 3})
-{3, 4}
-</pre>
+&gt;&gt;&gt; s.intersection({6, 5, 4, 3})
+{3, 4, 5}
+</code></pre>
 
-<p>For more detail on Sets you can go to the following link: <a href="http://docs.python.org/py3k/library/stdtypes.html#set">PythonSets</a></p>
+<p>For more detail on Sets you can go to the following link:
+<a href="http://docs.python.org/py3k/library/stdtypes.html#set">PythonSets</a></p>
 
-<h3>Question 1:</h3>
+<p><strong>Problem 1</strong>: Implement the union function for sets. Union takes in two sets,
+and returns a new set with elements from the first set, and all other elements
+that have not already have been seen in the second set.</p>
 
-<p>Implement the union function for sets. Union takes in two sets, and returns a new set with elements from the first set, and all other elements that have not already have been seen in the second set.</p>
+<pre><code>def union(s1, s2):
+    """Returns the union of two sets.
 
-<pre>
->>> r = {0, 6, 6}
->>> s = {1, 2, 3, 4}
->>> t = union(s, {1, 6})
-{1, 2, 3, 4, 6}
->>> union(r, t)
-{0, 1, 2, 3, 4, 6}
-</pre>
-
-<p></html></p>
+    &gt;&gt;&gt; r = {0, 6, 6}
+    &gt;&gt;&gt; s = {1, 2, 3, 4}
+    &gt;&gt;&gt; t = union(s, {1, 6})
+    &gt;&gt;&gt; t
+    {1, 2, 3, 4, 6}
+    &gt;&gt;&gt; union(r, t)
+    {0, 1, 2, 3, 4, 6}
+    """
+    "*** YOUR CODE HERE ***"
+</code></pre>
 
 <?php if ($CUR_DATE > $RELEASE_DATE) { ?>
   <button id="toggleButton0">Toggle Solution</button>
   <div id="toggleText0" style="display: none">
-    <p>def union(s1,s2):
-    union<em>set = set()
+    <pre><code>def union(s1,s2):
+    union_set = set()
     for elem in s1:
-        union</em>set.add(elem)
+        union_set.add(elem)
     for elem in s2:
-        union<em>set.add(elem)
-    return union</em>set</p>
+        union_set.add(elem)
+    return union_set
+</code></pre>
 
   </div>
 <?php } ?>
-<h3>Question 2:</h3>
+<p><strong>Problem 2</strong>: Implement the intersection function for two sets. Intersection
+takes in two sets and returns a new set of only the elements in both sets.</p>
 
-<p>Implement the intersection function for two sets, intersection takes in two sets and returns a new set of only the elements in both sets</p>
+<pre><code>def intersection(s1, s2):
+    """Returns the intersection of two sets.
 
-<pre>
->>> r = {0, 1, 4,0}
->>> s = {1, 2, 3, 4}
->>> t = intersection(s, {3,4,2})
-{2,3,4}
->>> intersection(r, t)
-{4}
-</pre>
-
-<p></html></p>
+    &gt;&gt;&gt; r = {0, 1, 4, 0}
+    &gt;&gt;&gt; s = {1, 2, 3, 4}
+    &gt;&gt;&gt; t = intersection(s, {3, 4, 2})
+    &gt;&gt;&gt; t
+    {2, 3, 4}
+    &gt;&gt;&gt; intersection(r, t)
+    {4}
+    """
+    "*** YOUR CODE HERE ***"
+</code></pre>
 
 <?php if ($CUR_DATE > $RELEASE_DATE) { ?>
   <button id="toggleButton1">Toggle Solution</button>
   <div id="toggleText1" style="display: none">
-    <p>def intersection(s1,s2):
-    intersection<em>set = set()
+    <pre><code>def intersection(s1, s2):
+    intersection_set = set()
     for elem in s1:
         if elem in s2:
-            intersection</em>set.add(elem)
-    return intersection_set</p>
+            intersection_set.add(elem)
+    return intersection_set
+</code></pre>
 
   </div>
 <?php } ?>
-<h2>Orders of Growth</h2>
+<h3>Orders of Growth</h3>
 
-<p>One really convinient thing about sets is that many operations on sets (adding elements, removing elements, checking membership) run in O(1) constant time. If you are interested on how, look up HashSets or look at the third challenge problem <a href="http://inst.eecs.berkeley.edu/~cs61a-td/">here</a>.</p>
+<p>One really convenient thing about sets is that many operations on sets (adding
+elements, removing elements, checking membership) run in O(1) (constant)
+time. If you are interested on how, look up HashSets or look at the third
+challenge problem <a href="http://inst.eecs.berkeley.edu/~cs61a-td/">here</a>.</p>
 
-<h3>Question 3</h3>
+<p><strong>Problem 3</strong>: Write the following function so it runs in O(n) time.</p>
 
-<p>Write the following function so it runs in O(n) time.</p>
+<pre><code>def extra_elem(a,b):
+    """B contains every element in A, and has one additional member, find
+    the additional member.
 
-<pre>
-def extra_elem(a,b):
-    '''B contains every element in A, and has one additional member, find the additional member'''
-    >>> extra_elem(["dog","cat","monkey"], ["dog", "cat", "monkey", "giraffe"])
-    "giraffe"
-    >>> extra_elem([1,2,3,4,5],[1,2,3,4,5,6])
+    &gt;&gt;&gt; extra_elem(['dog', 'cat', 'monkey'], ['dog', 'cat', 'monkey', 'giraffe'])
+    'giraffe'
+    &gt;&gt;&gt; extra_elem([1, 2, 3, 4, 5], [1, 2, 3, 4, 5, 6])
     6
-    *** your code here ***
-
-</pre>
-
-<p></html></p>
+    """
+    "*** YOUR CODE HERE ***"
+</code></pre>
 
 <?php if ($CUR_DATE > $RELEASE_DATE) { ?>
   <button id="toggleButton2">Toggle Solution</button>
   <div id="toggleText2" style="display: none">
-    <pre>
-def extra_elem(a,b):
-    '''B contains every element in A, and has one additional member, find the additional member'''
-    >>> extra_elem(["dog","cat","monkey"], ["dog", "cat", "monkey", "giraffe"])
-    "giraffe"
-    >>> extra_elem([1,2,3,4,5],[1,2,3,4,5,6])
-    6
-    return list(set(b) - set(a))[0]
-</pre>
+    <pre><code>def extra_elem(a,b):
+    """B contains every element in A, and has one additional member, find
+    the additional member.
 
-<p></html></p>
+    &gt;&gt;&gt; extra_elem(['dog', 'cat', 'monkey'], ['dog', 'cat', 'monkey', 'giraffe'])
+    'giraffe'
+    &gt;&gt;&gt; extra_elem([1, 2, 3, 4, 5], [1, 2, 3, 4, 5, 6])
+    6
+    """
+    return list(set(b) - set(a))[0]
+</code></pre>
 
   </div>
 <?php } ?>
-<h3>Question 4</h3>
+<p><strong>Problem 4</strong>: Write the following function so it runs in O(n) time.</p>
 
-<p>Write the following function so it runs in O(n) time.</p>
+<pre><code>def add_up(n, lst):
+    """Returns True if any two non identical elements in lst add up to any n.
 
-<pre>
-def add_up(n, lst):
-    ''' Returns true if any two non identical elements in lst add up to any n
-    >>> add_up(100,[1,2,3,4,5])
+    &gt;&gt;&gt; add_up(100, [1, 2, 3, 4, 5])
     False
-    >>> add_up(7,[1,2,3,4,2])
+    &gt;&gt;&gt; add_up(7, [1, 2, 3, 4, 2])
     True
-    >>> add_up(10,[5,5])
+    &gt;&gt;&gt; add_up(10, [5, 5])
     False
-    '''
-    *** your code here ***
-</pre>
-
-<p></html></p>
+    """
+    "*** YOUR CODE HERE ***"
+</code></pre>
 
 <?php if ($CUR_DATE > $RELEASE_DATE) { ?>
   <button id="toggleButton3">Toggle Solution</button>
   <div id="toggleText3" style="display: none">
-    <pre>
-def add_up(n, lst):
-    ''' Returns true if any two nonidentical elements in lst add up to n
-    >>> add_up(100,[1,2,3,4,5])
+    <pre><code>def add_up(n, lst):
+    """Returns True if any two non identical elements in lst add up to any n.
+
+    &gt;&gt;&gt; add_up(100, [1, 2, 3, 4, 5])
     False
-    >>> add_up(7,[1,2,3,4,2])
+    &gt;&gt;&gt; add_up(7, [1, 2, 3, 4, 2])
     True
-    >>> add_up(10,[5,5])
+    &gt;&gt;&gt; add_up(10, [5, 5])
     False
-    '''
+    """
     check_set = set()
     for elem in lst:
         check_set.add(n - elem)
     return bool(intersection(check_set, set(lst)))
-</pre>
-
-<p></html></p>
+</code></pre>
 
   </div>
 <?php } ?>
-<h3>Question 5</h3>
+<p><strong>Problem 5</strong>: Write the following function so it runs in O(n) time.</p>
 
-<p>Write the following function so it runs in O(n) time.</p>
+<pre><code>def find_duplicates(lst):
+    """Returns True if lst has any duplicates and False if it does not.
 
-<pre>
-def find_duplicates(lst):
-    ''' Returns true if lst has any duplicates and false if it does not
-    >>> find_duplicates([1,2,3,4,5])
+    &gt;&gt;&gt; find_duplicates([1, 2, 3, 4, 5])
     False
-    >>> find_duplicates([1,2,3,4,2])
+    &gt;&gt;&gt; find_duplicates([1, 2, 3, 4, 2])
     True
-    '''
-    *** your code here ***
-</pre>
-
-<p></html></p>
+    """
+    "*** YOUR CODE HERE ***"
+</code></pre>
 
 <?php if ($CUR_DATE > $RELEASE_DATE) { ?>
   <button id="toggleButton4">Toggle Solution</button>
   <div id="toggleText4" style="display: none">
-    <pre>
-def find_duplicates(lst):
-    ''' Returns true if lst has any duplicates and false if it does not
-    >>> find_duplicates([1,2,3,4,5])
-    False
-    >>> find_duplicates([1,2,3,4,2])
-    True
-    '''
-    return list(set(lst)) == lst
-</pre>
+    <pre><code>def find_duplicates(lst):
+    """Returns True if lst has any duplicates and False if it does not.
 
-<p></html></p>
+    &gt;&gt;&gt; find_duplicates([1, 2, 3, 4, 5])
+    False
+    &gt;&gt;&gt; find_duplicates([1, 2, 3, 4, 2])
+    True
+    """
+    return len(set(lst)) != len(lst)
+</code></pre>
 
   </div>
 <?php } ?>
-<h3>Question 6</h3>
+<p><strong>Problem 6</strong>: Write the following function so it runs in O(n) time.</p>
 
-<p>Write the following function so it runs in O(n) time.</p>
+<pre><code>def find_duplicates_k(k, lst):
+    """Returns True if there are any duplicates in lst that are within k
+    indices apart.
 
-<pre>
-def find_duplicates_k(k, lst):
-    ''' Returns true if there are any duplicates in lst that are within k indices apart
-    >>> find_duplicates_k(3,[1,2,3,4,1])
+    &gt;&gt;&gt; find_duplicates_k(3, [1, 2, 3, 4, 1])
     False
-    >>> find_duplicates_k(4,[1,2,3,4,1])
+    &gt;&gt;&gt; find_duplicates_k(4, [1, 2, 3, 4, 1])
     True
-    '''
-    *** your code here ***
-</pre>
-
-<p></html></p>
+    """
+    "*** YOUR CODE HERE ***"
+</code></pre>
 
 <?php if ($CUR_DATE > $RELEASE_DATE) { ?>
   <button id="toggleButton5">Toggle Solution</button>
   <div id="toggleText5" style="display: none">
-    <pre>
-def find_duplicates_k(k, lst):
-    ''' Returns true if there are any duplicates in lst that are within k indices apart '''
-    >>> find_duplicates(3,[1,2,3,4,1])
-    False
-    >>> find_duplicates(4,[1,2,3,4,1])
-    True
+    <pre><code>def find_duplicates_k(k, lst):
+    """Returns True if there are any duplicates in lst that are within k
+    indices apart.
 
+    &gt;&gt;&gt; find_duplicates_k(3, [1, 2, 3, 4, 1])
+    False
+    &gt;&gt;&gt; find_duplicates_k(4, [1, 2, 3, 4, 1])
+    True
+    """
     prev_set = set()
     for i, elem in enumerate(lst):
         if elem in prev_set:
             return True
         prev_set.add(elem)
-        if i - k >= 0:
+        if i - k &gt;= 0:
             prev_set.remove(lst[i - k])
     return False
-    '''
-</pre>
-
-<p></html></p>
+</code></pre>
 
   </div>
 <?php } ?>
-<h3>Question 7</h3>
+<p><strong>Problem 7</strong>: Write the following function so it runs in O(log n) time.</p>
 
-<p>Write the following function so it runs in O(log n) time.</p>
+<pre><code>def pow(n,k):
+    """Computes n^k.
 
-<pre>
-def pow(n,k)
-    ''' Computes n^k '''
-    >>> pow(2,3)
+    &gt;&gt;&gt; pow(2, 3)
     8
-    >>> pow(4,2)
+    &gt;&gt;&gt; pow(4, 2)
     16
-    *** your code here ***
-</pre>
-
-<p></html></p>
+    """
+    "*** YOUR CODE HERE ***"
+</code></pre>
 
 <?php if ($CUR_DATE > $RELEASE_DATE) { ?>
   <button id="toggleButton6">Toggle Solution</button>
   <div id="toggleText6" style="display: none">
-    <pre>
-def pow(n,k)
-    ''' Computes n^k
-    >>> pow(2,3)
+    <pre><code>def pow(n,k):
+    """Computes n^k.
+
+    &gt;&gt;&gt; pow(2, 3)
     8
-    >>> pow(4,2)
+    &gt;&gt;&gt; pow(4, 2)
     16
-    '''
+    """
     if k == 1:
         return n
     if k % 2 == 0:
         return pow(n*n,k//2)
     else:
         return n * pow(n*n, k//2)
-</pre>
-
-<p></html></p>
+</code></pre>
 
   </div>
 <?php } ?>
-<h3>Question 8</h3>
+<p><strong>Problem 8</strong>: Write the following function so it runs in O(n) time.</p>
 
-<p>Write the following function so it runs in O(n) time</p>
+<pre><code>def missing_no(lst):
+    """lst contains all the numbers from 0 to n for some n except some
+    number k. Find k.
 
-<pre>
-def missing_no(lst)
-    ''' lst contains all the numbers from 1 to n for some n except some number k, find k
-    >>> missing_no([1, 0, 4, 5, 7, 9, 2, 6, 3])
+    &gt;&gt;&gt; missing_no([1, 0, 4, 5, 7, 9, 2, 6, 3])
     8
-    >>> missing_no(list(filter(lambda x: x != 293, list(range(2000)))))
+    &gt;&gt;&gt; missing_no(list(filter(lambda x: x != 293, list(range(2000)))))
     293
-    '''
-    *** your code here ***
-</pre>
-
-<p></html></p>
+    """
+    "*** YOUR CODE HERE ***"
+</code></pre>
 
 <?php if ($CUR_DATE > $RELEASE_DATE) { ?>
   <button id="toggleButton7">Toggle Solution</button>
   <div id="toggleText7" style="display: none">
-    <pre>
-def missing_no(lst)
-    ''' lst contains all the numbers from 1 to n for some n except some number k, find k
-    >>> missing_no([1, 0, 4, 5, 7, 9, 2, 6, 3])
+    <pre><code>def missing_no(lst):
+    """lst contains all the numbers from 0 to n for some n except some
+    number k. Find k.
+
+    &gt;&gt;&gt; missing_no([1, 0, 4, 5, 7, 9, 2, 6, 3])
     8
-    >>> missing_no(list(filter(lambda x: x != 293, list(range(2000)))))
+    &gt;&gt;&gt; missing_no(list(filter(lambda x: x != 293, list(range(2000)))))
     293
-    '''
-    return sum(range(max(lst)+1)) - sum(lst)
-</pre>
-
-<p></html>
-~solution</p>
-
-<h3>Question 9</h3>
-
-<p>Write the following function so it runs in O(n) time.</p>
-
-<pre>
-def find_duplicates_k_l(k,l, lst):
-    ''' Returns true if there are any two values who in lst that are within k indices apart AND if the absolute value of their difference is less than or equal to l.
-    >>> find_duplicates_k_l(4,0,[1,2,3,4,5])
-    False
-    >>> find_duplicates_k_l(4,1,[1,2,3,4,5])
-    True
-    >>> find_duplicates_k_l(4,0,[1,2,3,4,1])
-    True
-    >>> find_duplicates_k_l(2,0[1,2,3,4,1])
-    False
-    >>> find_duplicates_k_l(1,100[100,275,320,988,27])
-    False
-    >>> find_duplicates_k_l(1,100[100,199,275,320,988,27])
-    True
-    >>> find_duplicates_k_l(1,100[100,23,199,275,320,988,27])
-    False
-    >>> find_duplicates_k_l(2,100[100,23,199,275,320,988,27])
-    True
-    '''
-    *** your code here ***
-</pre>
+    """
+    return sum(range(max(lst) + 1)) - sum(lst)
+</code></pre>
 
   </div>
 <?php } ?>
+<p><strong>Problem 9</strong>: Write the following function so it runs in O(n) time.</p>
+
+<pre><code>def find_duplicates_k_l(k, l, lst):
+    """Returns True if there are any two values who in lst that are within k
+    indices apart AND if the absolute value of their difference is less than
+    or equal to l.
+
+    &gt;&gt;&gt; find_duplicates_k_l(4, 0, [1, 2, 3, 4, 5])
+    False
+    &gt;&gt;&gt; find_duplicates_k_l(4, 1, [1, 2, 3, 4, 5])
+    True
+    &gt;&gt;&gt; find_duplicates_k_l(4, 0, [1, 2, 3, 4, 1])
+    True
+    &gt;&gt;&gt; find_duplicates_k_l(2, 0, [1, 2, 3, 4, 1])
+    False
+    &gt;&gt;&gt; find_duplicates_k_l(1, 100, [100, 275, 320, 988, 27])
+    False
+    &gt;&gt;&gt; find_duplicates_k_l(1, 100, [100, 199, 275, 320,988,27])
+    True
+    &gt;&gt;&gt; find_duplicates_k_l(1, 100, [100, 23, 199, 275,320,988,27])
+    False
+    &gt;&gt;&gt; find_duplicates_k_l(2, 100, [100, 23, 199, 275,320,988,27])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+</code></pre>
 
   </body>
   <?php if ($CUR_DATE > $RELEASE_DATE) { ?>
