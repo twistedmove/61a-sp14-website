c51acfbf008634472c3d32b1e1b74e9f20110058
Brian Hou <brian.hou@live.com>
2013-11-26 14:43:54 -0800

    Update lab11 logic to composing programs

--- lab/lab11/logic/logic.py
+++ lab/lab11/logic/logic.py
@@ -17,54 +17,17 @@ from ucb import main, trace
 
 import scheme
 import scheme_reader
-import scheme_test
 
 facts = []
-debug = False
 
 #############
 # Inference #
 #############
 
-class SpecialFrame(Frame):
-    def __init__(self, parent):
-        Frame.__init__(self, parent)
-        self.diffs = {}
-
-    def diff(self, sym, val):
-        debug_print("{} diff {}".format(sym, val))
-        self.diffs[sym] = val
-
-    def all_diffs(self):
-        rval = {}
-        for k, v in self.diffs.items():
-            rval[k] = v
-
-        if self.parent:
-            for k, v in self.parent.all_diffs().items():
-                rval[k] = v
-
-        return rval
-
-    def define(self, sym, val):
-        for sym, val in self.all_diffs().items():
-            evaled = lookup(sym, self)
-            stored = lookup(val, self)
-            if evaled == stored:
-                debug_print("{} is equal to {}".format(evaled, stored))
-                return False
-        Frame.define(self, sym, val)
-        return True
-
 def do_query(clauses):
     """Yield all bindings that simultaneously satisfy clauses."""
-    for env in search(clauses, SpecialFrame(None), 0):
-        rval = [(v, ground(v, env)) for v in get_vars(clauses)]
-        yield rval
-
-def debug_print(*s):
-    if debug:
-        print(*s)
+    for env in search(clauses, Frame(None), 0):
+        yield [(v, ground(v, env)) for v in get_vars(clauses)]
 
 DEPTH_LIMIT = 20
 def search(clauses, env, depth):
@@ -74,33 +37,11 @@ def search(clauses, env, depth):
     if clauses is nil:
         yield env
     elif DEPTH_LIMIT is None or depth <= DEPTH_LIMIT:
-        debug_print("Searching", clauses, "with", "")
-        if clauses.first != nil:
-            if clauses.first.first == "diff":
-                items = clauses.first.second
-                first = lookup(items.first, env)
-                second = lookup(items.second.first, env)
-                debug_print("Diff {} {}".format(first, second))
-                if lookup(first, env) == lookup(second, env):
-                    return
-
-                if isvar(first):
-                    env.diff(first, second)
-                elif isvar(second):
-                    env.diff(second, first)
-
-                for item in search(clauses.second, env, depth):
-                    yield item
-                return
-
         for fact in facts:
-            debug_print("Trying", fact, "for", clauses.first, "env", "")
             fact = rename_variables(fact, get_unique_id())
-            env_head = SpecialFrame(env)
+            env_head = Frame(env)
             if unify(fact.first, clauses.first, env_head):
-                debug_print("{} worked on {} env {}".format(fact.first, clauses.first, env_head))
                 for env_rule in search(fact.second, env_head, depth+1):
-                    # debug_print("found {} for search for {}".format(env_rule, fact.second))
                     for result in search(clauses.second, env_rule, depth+1):
                         yield result
 
@@ -108,15 +49,16 @@ def unify(e, f, env):
     """Destructively extend env so as to unify (make equal) e and f, returning
     True if this succeeds and False otherwise.  env may be modified in either
     case (its existing bindings are never changed)."""
-    # debug_print("unifying {} and {} env {}".format(e, f, env))
     e = lookup(e, env)
     f = lookup(f, env)
     if e == f:
         return True
     elif isvar(e):
-        return env.define(e, f)
+        env.define(e, f)
+        return True
     elif isvar(f):
-        return env.define(f, e)
+        env.define(f, e)
+        return True
     elif scheme_atomp(e) or scheme_atomp(f):
         return False
     else:
@@ -145,10 +87,6 @@ def ground(expr, env):
         return Pair(ground(expr.first, env), ground(expr.second, env))
     else:
         return expr
-
-#####################
-# Support functions #
-#####################
 
 def get_vars(expr):
     """Return all logical vars in expr as a list."""
@@ -190,6 +128,10 @@ def isvar(symbol):
 
 def process_input(expr, env):
     """Process an input expr, which may be a fact or query."""
+    if expr == 'exit':
+        print("Goodbye")
+        sys.exit(0)
+
     if not scheme_listp(expr):
         print('Improperly formed expression.')
     elif expr.first in ("fact", "!"):
@@ -201,9 +143,9 @@ def process_input(expr, env):
             if not success:
                 print('Success!')
             success = True
-            result_str = "\t".join("{0}: {1}".format(k[1:], v) for k, v in result)
-            if result_str:
-                print(result_str)
+            output = "\t".join("{0}: {1}".format(k[1:], v) for k, v in result)
+            if output:
+                print(output)
         if not success:
             print('Failed.')
     elif expr.first == "load":
@@ -214,9 +156,6 @@ def process_input(expr, env):
 @main
 def run(*argv):
     scheme_reader.buffer_input.__defaults__ = ('logic> ',)
-    scheme_reader.buffer_lines.__defaults__ = ('logic> ',)
+    scheme_reader.buffer_lines.__defaults__ = ('logic> ', False)
     scheme.scheme_eval = process_input
-    if argv and argv[0] == '-test':
-        scheme_test.run_tests(*argv[1:])
-    else:
-        scheme.run(*argv)
+    scheme.run(*argv)
