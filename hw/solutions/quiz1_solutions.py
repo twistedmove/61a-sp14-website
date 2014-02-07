99bfbea63d4624ab879c62369250c9486abe3ea1
Paul Hilfinger <Hilfinger@cs.berkeley.edu>
2014-02-06 17:23:24 -0800

    Update quiz. Add images for Uejio, Zuccarini. Rename Yang image. Update lecture 7 from class. Add readers to staff page.

--- hw/solutions/quiz1_solutions.py
+++ hw/solutions/quiz1_solutions.py
@@ -1,82 +1,88 @@
-# Name:
-# Email:
+#  Name:
+#  Email:
 
 # Q1.
 
-def triangular(x, y, z):
-    """Return whether x, y, and z are triangular.
+def overlaps(low0, high0, low1, high1):
+    """Return whether the open intervals (LOW0, HIGH0) and (LOW1, HIGH1)
+    overlap.
 
-    >>> triangular(3, 4, 5)
+    >>> overlaps(10, 15, 14, 16)
     True
-    >>> triangular(3, 14, 5) # 14 is greater than 3 + 5
+    >>> overlaps(10, 15, 1, 5)
     False
-    >>> triangular(7.5, 3.5, 4) # 7.5 is equal to 3.5 + 4
+    >>> overlaps(10, 10, 9, 11)
     False
-    >>> result = triangular(5, 4, 3) # Return, don't print
+    >>> result = overlaps(1, 5, 0, 3)  # Return, don't print
     >>> result
     True
-    >>> triangles = [(1, 2, 2), (2, 1, 2), (2, 2, 1), (2, 2, 2), (4, 3, 5),
-    ... (3, 5, 4), (5, 3, 4), (4, 5, 3), (5, 4, 3)]
-    >>> [triangular(*t) for t in triangles].count(False)
+    >>> [overlaps(a0, b0, a1, b1) for a0, b0, a1, b1 in 
+    ...       ( (1, 4, 2, 3), (1, 4, 0, 2), (1, 4, 3, 5), (0.1, 0.4, 0.2, 0.3),
+    ...         (2, 3, 1, 4), (0, 2, 1, 4), (3, 5, 1, 4) )].count(False)
     0
-    >>> lines = [(1, 2, 3), (2, 1, 3), (1, 3, 2), (3, 1, 2), (2, 3, 1),
-    ... (3, 2, 1)]
-    >>> [triangular(*t) for t in lines].count(True)
-    0
-    >>> other = [(0.1, 0.1, 1), (0.1, 1, 0.1), (1, 0.1, 0.1)]
-    >>> [triangular(*t) for t in other].count(True)
+    >>> [overlaps(a0, b0, a1, b1) for a0, b0, a1, b1 in 
+    ...       ( (1, 4, -1, 0), (1, 4, 5, 6), (1, 4, 4, 5), (1, 4, 0, 1),
+    ...         (-1, 0, 1, 4), (5, 6, 1, 4), (4, 5, 1, 4), (0, 1, 1, 4),
+    ...         (5, 5, 3, 6), (5, 3, 4, 6), (5, 5, 5, 5), 
+    ...         (3, 6, 5, 5), (4, 6, 5, 3), (0.3, 0.6, 0.5, 0.5) )].count(True)
     0
     """
-    return x + y + z > 2 * max(x, y, z)
+    return low1 < min(high0, high1) > low0
 
 # Q2.
 
-def next_square(x):
-    """Return the smallest perfect square greater than x.
+def last_square(x):
+    """Return the largest perfect square less than X, where X>0.
 
-    >>> next_square(10)
-    16
-    >>> next_square(39)
-    49
-    >>> next_square(100)
-    121
-    >>> result = next_square(2) # Return, don't print
+    >>> last_square(10)
+    9
+    >>> last_square(39)
+    36
+    >>> last_square(100)
+    81
+    >>> result = last_square(2) # Return, don't print
     >>> result
-    4
-    >>> cases = [(1, 4), (2, 4), (3, 4), (4, 9), (8, 9), (9, 16), (15, 16),
-    ... (16, 25), (24, 25), (25, 36), (26, 36)]
-    >>> [next_square(s) == t for s, t in cases].count(False)
+    1
+    >>> cases = [(1, 0), (2, 1), (3, 1), (4, 1), (5, 4), (6, 4), 
+    ...          (10, 9), (17, 16), (26, 25), (36, 25), (46, 36)]
+    >>> [last_square(s) == t for s, t in cases].count(False)
     0
     """
-    k = 1
-    while k * k <= x:
+    k = 0
+    while k * k < x:
         k = k + 1
-    return k * k
+    return (k-1) * (k-1)
 
 # Q3.
 
-def digit_span(x):
-    """Return the difference between x's largest and smallest digits.
+def ordered_digits(x):
+    """Return True if the (base 10) digits of X>0 are in non-decreasing
+    order, and False otherwise.
 
-    >>> digit_span(2013) # 3 - 0 = 3
-    3
-    >>> digit_span(75) # 7 - 5 = 2
-    2
-    >>> digit_span(2345678765432) # 8 - 2 = 6
-    6
-    >>> result = digit_span(6473465) # Return, don't print
+    >>> ordered_digits(5)
+    True
+    >>> ordered_digits(11)
+    True
+    >>> ordered_digits(127)
+    True
+    >>> ordered_digits(1357)
+    True
+    >>> ordered_digits(21)
+    False
+    >>> result = ordered_digits(1375) # Return, don't print
     >>> result
-    4
-    >>> cases = [(1084, 8), (3105, 5), (7000, 7), (174, 6), (714, 6),
-    ... (147, 6), (121, 1), (33553355335544664466446644, 3)]
-    >>> [digit_span(s) == t for s, t in cases].count(False)
+    False
+    >>> cases = [(1, True), (9, True), (10, False), (11, True), (32, False),
+    ...          (23, True), (99, True), (111, True), (122, True), (223, True),
+    ...          (232, False), (999, True),
+    ...          (13334566666889, True), (987654321, False)]
+    >>> [ordered_digits(s) == t for s, t in cases].count(False)
     0
     """
-    largest, smallest = x % 10, x % 10
-    x = x // 10
-    while x > 0:
-        largest = max(largest, x % 10)
-        smallest = min(smallest, x % 10)
+    last = x % 10
+    val = x // 10
+    while x > 0 and last >= x % 10:
+        last = x % 10
         x = x // 10
-    return largest - smallest
+    return x == 0
 
