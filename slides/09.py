a16bc9219d8facc0915e431e479e83acb15e9570
Paul Hilfinger <Hilfinger@cs.berkeley.edu>
2014-02-10 14:00:15 -0800

    Update lecture 9.

--- slides/09.py
+++ slides/09.py
@@ -1,6 +1,85 @@
 # Test-driven development
 
 from ucb import trace, interact
+from turtle import *
+from math import sqrt
+
+def occurs(x, pair):
+    """X occurs at least once in numeric pair PAIR.
+
+    >>> occurs(3, ())
+    False
+    >>> occurs(3, (2, 3))
+    True
+    >>> occurs(3, (3, 2))
+    True
+    >>> occurs(3, (1, 2))
+    False
+    >>> occurs(3, ((1, 2), ((), (2, ((), 3)))))
+    True
+    """
+    if x == pair:
+        return True
+    
+    elif pair == () or type(pair) is int:
+        return False
+    else:
+        return occurs(x, pair[0]) or occurs(x, pair[1])
+
+def first_leaf(pair):
+    """The first leaf in PAIR, reading left to right.
+    >>> first_leaf(())
+    ()
+    >>> first_leaf(5)
+    5
+    >>> first_leaf((((3, ()), (2, 1)), ()))
+    3
+    >>> first_leaf(((((), 3), (2, 1)), ()))
+    ()
+    """
+
+    if type(pair) is int or pair == ():
+        return pair
+    else:
+        return first_leaf(pair[0])
+
+def sierpinski(x, y, side, depth):
+    """Draw a Sierpinski triangle of given DEPTH with given SIDE and
+    lower-left corner at (X, Y).  A depth of 0 draws a triangle,
+    a depth of 1 draws a triangle of three triangles, etc."""
+
+    def triangle(x, y, side):
+        """Assuming current heading is 30, draw a filled triangle with
+        its lower-left corner at (X, Y), and given SIDE."""
+        penup()
+        goto(x, y)
+        pendown()
+        begin_fill()
+        setheading(30)
+        forward(side)
+        right(120)
+        forward(side)
+        right(120)
+        forward(side)
+        end_fill()
+
+    if depth == 0:
+        triangle(x, y, side)
+    else:
+        height = 0.25 * sqrt(3) * side
+
+        sierpinski(x, y, side/2, depth-1)
+        sierpinski(x + side/4, y + height, side/2, depth-1)
+        sierpinski(x + side/2, y, side/2, depth-1)
+
+def draw_triangle(x, y, side, depth):
+    """Draw a Sierpinski triangle of given SIDE and DEPTH with lower-left
+    corner at (X, Y) after first clearing the screen and initializing the
+    graphics package."""
+    mode('logo')
+    clear()
+    sierpinski(x, y, side, depth)
+    
 
 def gcd(m, n):
     """Return the largest k that evenly divides both m and n.
@@ -50,3 +129,4 @@ def sum_squares_up_to(n):
         total, k = total + square(k), k + 1
     return total
 
+interact()
