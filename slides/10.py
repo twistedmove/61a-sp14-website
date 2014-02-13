322e373dda842054f828d7271e907fbc17c2f0ec
Paul Hilfinger <Hilfinger@cs.berkeley.edu>
2014-02-12 16:51:48 -0800

    Updates to lecture 10.  Modify schedule title for lecture 10.

--- slides/10.py
+++ slides/10.py
@@ -1,35 +1,14 @@
-# Numeric Data Types
-
-type(2)
-type(1.5)
-type(1+1j)
-True + True
-type(-True)
-1/3 == 0.333333333333333300000  # Beware of approximations
-from math import pi, tan
-tan(pi/4)
-
-# Dates
-
-from datetime import date
-today = date(2013, 9, 25)
-freedom = date(2013, 12, 20)
-str(freedom - today)
-today.year
-today.strftime('%A, %B %d')
-type(today)
-
 # Rational arithmetic
 
 def add_rational(x, y):
     """Add rational numbers x and y."""
     nx, dx = numer(x), denom(x)
     ny, dy = numer(y), denom(y)
-    return rational(nx * dy + ny * dx, dx * dy)
+    return make_rat(nx * dy + ny * dx, dx * dy)
 
 def mul_rational(x, y):
     """Multiply rational numbers x and y."""
-    return rational(numer(x) * numer(y), denom(x) * denom(y))
+    return make_rat(numer(x) * numer(y), denom(x) * denom(y))
 
 def equal_rational(x, y):
     """Return whether rational numbers x and y are equal."""
@@ -37,19 +16,17 @@ def equal_rational(x, y):
 
 # Constructor and selectors
 
-from operator import getitem
-
-def rational(n, d):
+def make_rat(n, d):
     """Construct a rational number x that represents n/d."""
     return (n, d)
 
-def numer(x):
-    """Return the numerator of rational number x."""
-    return getitem(x, 0)
+def numer(r):
+    """Return the numerator of rational number R."""
+    return r[0]
 
-def denom(x):
-    """Return the denominator of rational number x."""
-    return getitem(x, 1)
+def denom(r):
+    """Return the denominator of rational number R."""
+    return r[1]
 
 
 # String conversion
@@ -62,7 +39,7 @@ def rational_to_string(x):
 # Improved constructor
 
 from fractions import gcd
-def rational(n, d):
+def make_rat(n, d):
     """Construct a rational number x that represents n/d in lowest terms."""
     g = gcd(n, d)
     return (n//g, d//g)
@@ -82,3 +59,17 @@ def pair(x, y):
 def getitem_pair(p, i):
     """Return the element at index i of pair p."""
     return p(i)
+
+# Using functional pairs:
+def make_rat(n, d):
+    g = gcd(n, d)
+    return pair(n//g, d//g)
+
+def numer(r):
+    """Return the numerator of rational number R."""
+    return r(0)
+
+def denom(r):
+    """Return the denominator of rational number R."""
+    return r(1)
+
