9c549fe7614778144563ed6737a92d9a1f802179
Paul Hilfinger <Hilfinger@cs.berkeley.edu>
2014-02-14 15:38:48 -0800

    Update 11.py

--- slides/11.py
+++ slides/11.py
@@ -1,104 +1,119 @@
-# Tuples are sequences
+# Rlists
 
-odds = (41, 43, 47, 49)
-len(odds)
-odds[1]
-odds[0] * odds[3] + len(odds)
-odds[odds[3]-odds[2]]
-
-# Recursive list abstract data type
-
+# The empty rlist (unique).
 empty_rlist = None
-
 def rlist(first, rest = empty_rlist):
     """A recursive list, r, such that first(R) is FIRST and 
     rest(R) is REST, which must be an rlist."""
-    return (first, rest)
-
+    return first, rest
 def first(r):
     """The first item in R."""
     return r[0]
-
 def rest(r):
     """The tail of R: the sequence consisting of items 1, 2,...,
     renumbered from 0."""
     return r[1]
 
-### +++ === ABSTRACTION BARRIER === +++ ###
 
-# Recursive list examples
+# The sequence 1, 3, 0, 4
+L = rlist(1, rlist(3, rlist(0, rlist(4, empty_rlist))))
+# The sequence containing sequences (0, 1) and (2, 3)
+L = rlist(rlist(0, rlist(1, empty_list)),
+          rlist(rlist(2, rlist(3, empty_list)),
+                empty_rlist))
 
-counts = rlist(1, rlist(2, rlist(3, rlist(4, empty_rlist))))
-alts = rlist(1, rlist(2, rlist(1, rlist(2, empty_rlist))))
-both = rlist(alts, rlist(counts, empty_rlist))
+# Length
 
-# Which of these evaluates to 3?
-# first(rest(rest(first(rest(both)))))
-# first(rest(first(rest(rest(both)))))
-# first(rest(first(rest(first(both)))))
-# first(rest(rest(rest(first(both)))))
-# first(first(rest(rest(first(both)))))
+def len_rlist(s):               # A sequence is:
+    """The length of rlist S."""
+    if s == empty_rlist:        # * Empty or...
+        return 0
+    else:
+        return 1 + len_rlist(rest(s)) 
+                                # A first element and
+                                # the rest of the list
+# Tail-recursive version
 
-# Implementing the sequence abstraction
+def len_rlist(s):            
+    """The length of rlist S."""
+    def len(sofar, s):
+        """Return SOFAR + the length of rlist S."""
+        if s == empty_rlist: 
+            return sofar
+        else:
+            return len(sofar + 1, rest(s))
+    len(0, s)
+
+# Iterative version
 
 def len_rlist(s):
-    """The length of rlist S.
-    >>>len_rlist(empty_rlist)
-    0
-    >>>len_rlist(rlist(1, rlist(4, empty_rlist)))
-    2
-    """
-
     sofar = 0
     while s != empty_rlist:
         sofar, s = sofar+1, rest(s)
     return sofar
 
-def getitem_rlist(s, i):
-    """Return the element at index i of recursive list s.
+# getitem
 
-    >>> getitem_rlist(alts, 3)
-    2
-    """
-    while i > 0:
-        s, i = rest(s), i - 1
-    return first(s)
+def getitem_rlist(s, k):
+    """Return the element at index K of recursive list S.
+    Assumes K >= 0.
+    >>> getitem_rlist(rlist(2, rlist(3, rlist (4))), 1)
+    3"""
 
-# Recursive implementation
-
-def len_rec(s):
-    """Return the length of recursive list s.
-
-    >>> len_rec(counts)
-    4
-    """
-    if s == empty_rlist:
-        return 0
-    else:
-        return 1 + len_rec(rest(s))
-
-def getitem_rec(s, i):
-    """Return the element at index i of recursive list s.
-
-    >>> getitem_rec(alts, 3)
-    2
-    """
-    if i == 0:
+    if k == 0:
         return first(s)
     else:
-        return getitem_rec(rest(s), i-1)
+        return getitem_rlist(rest(s), k-1)
 
-def reverse(s):
-    """Return s reversed.
+# Iterative version
 
-    >>> r = rlist(4, rlist(3, rlist(2, rlist(1, empty_rlist))))
-    >>> reverse(counts) == r
-    True
-    """
-    return reverse_to(s, empty_rlist)
+def getitem_rlist(s, k):
+    """Return the element at index K of recursive list S.
+    Assumes K >= 0."""
 
-def reverse_to(s, result):
+    while k != 0:
+        s, k = rest(s), k-1
+    return first(s)
+
+# Map
+
+def square_rlist(s):
+    """The list of squares of the elements of rlist S."""
     if s == empty_rlist:
-        return result
+        return empty_rlist:
     else:
-        return reverse_to(rest(s), rlist(first(s), result))
+        return rlist(first(s)**2, square_rlist(rest(s)))
+def map_rlist(f, s):
+    """The list of values F(x) for each element x of S in order."""
+    if s == empty_rlist:
+        return empty_rlist
+    else:  
+        return rlist(f(first(s)), map_rlist(f, rest(s)))
+
+# Alternative definition
+
+def square_rlist2(s):
+    return map_rlist(lambda x: x*x, s)
+
+# Extend
+
+def extend_rlist(left, right):
+    """The sequence of items of rlist `left'
+    followed by the items of `right'."""
+    if left == empty_rlist:
+         return right
+    else:
+         return rlist(first(left), extend_rlist(rest(left), right))
+
+# Reverse
+
+def reverse_rlist(L):
+    def reverse_extend(to_do, already_done):
+        """The result of extending ALREADY_DONE with
+        the reverse of TO_DO."""
+        if to_do == empty_rlist:
+            return empty_rlist
+        else:
+            return reverse_extend(rest(to_do), 
+                                  rlist(first(to_do), already_done))
+    reverse_extend(L, empty_rlist)
