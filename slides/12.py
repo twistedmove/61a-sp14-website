1e04d95625cce07d48e1bc8cd62b192ad4c585c9
Paul Hilfinger <Hilfinger@cs.berkeley.edu>
2014-02-21 20:08:53 -0800

    More corrections to lecture 12 slides. Update 12.py with current code.

--- slides/12.py
+++ slides/12.py
@@ -1,93 +1,57 @@
-# Sequence iteration
-
-stairs = (1, 2, 3, 4, 5, 4, 3, 4, 5, 6, 7)
-
-from operator import getitem
-
-def count_while(s, value, getitem=getitem, len=len):
-    """Count the number of occurrences of value in sequence s.
-
-    >>> stairs.count(4)
-    3
-    >>> count_while(stairs, 4)
-    3
+from operator import *
+def reduce(f, seq, init):
+    """If SEQ is a sequence of length n>=0, returns sn, where
+    s0 = INIT, s1=F(s0, SEQ[0]), s2=F(s1, SEQ[1]), ...
+    >>> L = (2, 3, 4)
+    >>> reduce(add, L, 0)
+    9
+    >>> reduce(mul, L, 1)
+    24
+    >>> reduce(lambda x, y: rlist(y, x), L, empty_rlist)
+    (4, (3, (2, None)))
     """
-    total, index = 0, 0
-    while index < len(s):
-        if getitem(s, index) == value:
-            total = total + 1
-        index = index + 1
-    return total
+    result = init
+    for p in seq:
+        result = f(result, p)
+    return result
 
-def count(s, value):
-    """Count the number of occurrences of value in sequence s.
 
-    >>> count(stairs, 4)
-    3
+def map(f, seq):
+    """Assuming SEQ is the sequence containing s1, s2, ..., sn, 
+    returns the tuple (F(s1), F(s2), ..., F(sn))."""
+    result = ()
+    for p in seq:
+        result = result + (f(p), )
+    return result
+
+def filter(pred, seq):
+    """Assuming SEQ is the sequence containing s1, s2, ..., sn, 
+    returns tuple containing only those si for which PRED(si) is true."""
+    result = ()
+    for p in seq:
+        result = result + (p,) if pred(p) else result
+    return result
+
+# Or...
+def filter(pred, seq):
+    return (x for x in seq if pred(x))
+
+def partition(L, x):
+    """Returns result of rearranging the elements of L so that 
+    all items < X appear before all items >= X, 
+    and all are otherwise in their original order."""
+    >>> L = (0, 9, 6, 2, 5, 11, 1)
+    >>> partition(L, 5)
+    (0, 2, 1, 9, 6, 5, 11)
     """
-    total = 0
-    for elem in s:
-        if elem == value:
-            total = total + 1
-    return total
+    return tuple((y in L if y < x)) + tuple((y in L if y >= x))
 
-def count_same(pairs):
-    """Count the number of pairs that contain the same value repeated.
-
-    >>> pairs = ((1, 2), (2, 2), (2, 3), (4, 4))
-    >>> count_same(pairs)
-    2
+def collapse_runs(L):
+    """Return result of removing the second and subsequent consecutive
+    duplicates of values in L,
+    >>> x = (1, 2, 2, 1, 1, 1, 2, 0, 0)
+    >>> collapse_runs(x)
+    (1, 2, 1, 2, 0)
     """
-    same_count = 0
-    for x, y in pairs:
-        if x == y:
-            same_count = same_count + 1
-    return same_count
-
-few = range(-2, 2)
-many = range(-2, 20000000)
-
-# List mutation
-
-suits = ['coin', 'string', 'myriad']  # A list literal
-suits.pop()             # Removes and returns the final element
-suits.remove('string')  # Removes the first element that equals the argument
-suits.append('cup')              # Add an element to the end
-suits.extend(['sword', 'club'])  # Add all elements of a list to the end
-suits[2] = 'spade'  # Replace an element
-suits
-suits[0:2] = ['heart', 'diamond']  # Replace a slice
-[suit.upper() for suit in suits]
-[suit[1:4] for suit in suits if len(suit) == 5]
-
-# Dictionaries
-
-numerals = {'I': 1.0, 'V': 5, 'X': 10}
-numerals['X']
-numerals['I'] = 1
-numerals['L'] = 50
-numerals
-sum(numerals.values())
-dict([(3, 9), (4, 16), (5, 25)])
-numerals.get('A', 0)
-numerals.get('V', 0)
-{x: x*x for x in range(3,6)}
-
-# {[1]: 2}
-# {1: [2]}
-# {([1], 2): 3}
-# {tuple([1, 2]): 3}
-
-# Identity and equality
-
-def ident():
-    suits = ['heart', 'diamond']
-    s = suits
-    t = list(suits)
-    suits += ['spade', 'club']
-    t[0] = suits
-    print(t)
-    suits.append('Joker')
-    print(t)
-    t[0].pop()
-    print(s)
+    
+    return tuple( (L[k] for k in range(len(L)) if k==0 or L[k-1]!=L[k]) )
