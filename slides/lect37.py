a08b79c28a8ee739f9450f94fa82f16cc6052d9b
Paul Hilfinger <Hilfinger@cs.berkeley.edu>
2014-04-30 20:17:59 -0700

    Release lecture materials

--- slides/lect37.py
+++ slides/lect37.py
@@ -5,5 +5,16 @@ def tree_path_gen(tree, path):
     """A generator of the items of TREE along the path
     described by PATH, where a true value in PATH indicates
     a left child and a false value indicates a right child."""
+    while not tree.is_empty:
+        yield tree.label
+        if not path:
+            tree = empty_tree
+        else:
+            if path[0]:
+                tree = tree.left
+            else:
+                tree = tree.right
+            del path[0]
+
 
 
